790e72e67278559e5376d6e862f3bf5378c39de4
rotemn2006 <rotemneugeboren@hilma.tech>
2024-11-13 18:16:09 +0200

    photos

--- src/components/Singlealbum.jsx
+++ src/components/Singlealbum.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import Photos from "./Photos";
 
 export default function Singlealbum(props) {
+  const [displayPhotos, setDisplayPhotos] = useState(false);
   return (
     <>
-      <div className="album">
-        id: {props.album.id} <br />
-        title:{props.album.title}
+      <div>
+        <button
+          className="album"
+          onClick={() => setDisplayPhotos((prev) => !prev)}
+        >
+          id: {props.album.id} <br />
+          title:{props.album.title}
+        </button>
+        {displayPhotos && <Photos albumId={props.album.id} />}
       </div>
     </>
   );
